136fbc439b21d03cc74332924e96ecfe0a7442f8
henry <franzhenry46@gmail.com>
2024-04-01 15:34:12 -0300

    criando formulario e obtendo valores

--- calculator/models.py
+++ calculator/models.py
@@ -1,28 +1,23 @@
 from django.db import models
 
 
-class Consumer(models.Model):
-    name = models.CharField("Nome do Consumidor", max_length=128)
-    document = models.CharField("Documento(CPF/CNPJ)", max_length=14, unique=True)
-    zip_code = models.CharField("CEP", max_length=8, null=True, blank=True)
-    city = models.CharField("Cidade", max_length=128)
-    state = models.CharField("Estado", max_length=128)
-    consumption = models.IntegerField("Consumo(kWh)", blank=True, null=True)
-    distributor_tax = models.FloatField(
-        "Tarifa da Distribuidora", blank=True, null=True
+
+class Consumption(models.Model):
+    consumo_mes1 = models.FloatField()
+    consumo_mes2 = models.FloatField()
+    consumo_mes3 = models.FloatField()
+    tarifa_distribuidora = models.FloatField()
+    tipo_tarifa_choices = (
+        ('residencial', 'Residencial'),
+        ('comercial', 'Comercial'),
+        ('industrial', 'Industrial'),
     )
-    #  create the foreign key for discount rule model here
+    tipo_tarifa = models.CharField(max_length=20, choices=tipo_tarifa_choices)
 
+    # Resultados
+    economia_anual = models.FloatField(null=True, blank=True)
+    economia_mensal = models.FloatField(null=True, blank=True)
+    desconto_aplicado = models.FloatField(null=True, blank=True)
+    cobertura = models.FloatField(null=True, blank=True)
 
-# TODO: Create the model DiscountRules below
-"""Fields:
--> Consumer type  
--> Consumption range
--> Cover value
--> Discount value
-The first three fields should be a select with the values provided in the table
-defined in the readme of the repository. Discount should be numerical
-"""
-
-# TODO: You must populate the consumer table with the data provided in the file consumers.xlsx
-#  and associate each one with the correct discount rule
+    pass
--- calculator/views.py
+++ calculator/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Consumption
+from calculator_python import calculator
 
 # TODO: Your list view should do the following tasks
 """
@@ -28,3 +30,24 @@ this page must be provided in the main page.
 def view2():
     # Create the second view here.
     pass
+
+def home(request):
+    if request.method=="POST":
+        rate_one =  int(request.POST["rate_one"])
+        rate_two =  int(request.POST["rate_two"])
+        rate_three =  int(request.POST["rate_three"])
+        distributor_tax = float(request.POST['rate_value'])
+        tax_type = request.POST['rate_type']
+        
+        calculator([rate_one,rate_two,rate_three],distributor_tax,tax_type)
+        # calculadora = Consumption(
+        #     consumo_mes1=rate_one,
+        #     consumo_mes2=rate_two,
+        #     consumo_mes3=rate_three,
+        #     tarifa_distribuidora=rate_value,
+        #     tipo_tarifa=rate_type
+        # )
+        # calculadora.save()
+        return render(request,'home.html')
+    else:
+        return render(request,'home.html')
